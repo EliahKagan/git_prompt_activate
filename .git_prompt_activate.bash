2c07b1f93d78984fd32961d8a2661afffecdfae9
Eliah Kagan <degeneracypressure@gmail.com>
2019-06-05 11:50:40 -0400

    remove the unused prompt_suffix variable
    The other alternative, actually _using_ it as originally intended, would be
    difficult because of quoting issues. I doubt the resulting code, if correct,
    would be very readable. Better to repeat the few characters of the suffix.

--- .git_prompt_activate.bash
+++ .git_prompt_activate.bash
@@ -3,15 +3,13 @@
 # On Debian-based systems this is usually already sourced and needn't be again.
 # . /usr/lib/git-core/git-sh-prompt
 
-prompt_suffix='\$ ' # If PS1 ends this way, we'll add a part for git before it.
-
 git_prompt_part='$(__git_ps1 " (%s)")'
 case "$TERM" in xterm-color|*-256color) # See color_prompt in Debian's .bashrc.
     git_prompt_part='\[\033[36m\]'"$git_prompt_part"'\[\033[0m\]';;
 esac
 
-PS1="${PS1/%\\$ /$git_prompt_part\\$ }"
-unset prompt_suffix git_prompt_part
+PS1="${PS1/%\\$ /$git_prompt_part\\$ }" # Put Git info before a final $ if any.
+unset git_prompt_part
 
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWSTASHSTATE=1
